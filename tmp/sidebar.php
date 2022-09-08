3ee9168efe95eb9631e23226e023db78db21216c
starlight <sergeygirchev@gmail.com>
2022-09-08 15:43:13 +0300

    ADD: structure, style

--- tmp/sidebar.php
+++ tmp/sidebar.php
@@ -1,6 +1,6 @@
 <div>
     <?php if ( is_single() && ! $cp ) { kb_share_buttons(); }?>
-    <div id="content" <?php echo kava_get_container_classes( 'container d-flex flex-row-reverse pb-60' ); ?>>
-        <aside id="main-sidebar" <?php kava_secondary_content_class( array( 'widget-area col-lg-3' ) ); ?>>
+    <div id="content" <?php echo kava_get_container_classes( 'container d-flex flex-column flex-md-row-reverse pb-60 px-0' ); ?>>
+        <aside id="main-sidebar" <?php kava_secondary_content_class( array( 'widget-area col-sm-10 col-md-5 col-lg-4' ) ); ?>>
             <?php dynamic_sidebar( 'sidebar' ); ?>
-        </aside>
+        </aside>`
