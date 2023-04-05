0b84aa61d3cf3a44b386de439a29332936356b34
starlight <sergeygirchev@gmail.com>
2023-04-05 10:16:59 +0300

    ADD: spinner

--- jet-search/jet-ajax-search/global/spinner.php
+++ jet-search/jet-ajax-search/global/spinner.php
@@ -5,11 +5,7 @@
 ?>
 
 <div class="jet-ajax-search__spinner-holder">
-	<div class="jet-ajax-search__spinner">
-		<div class="rect rect-1"></div>
-		<div class="rect rect-2"></div>
-		<div class="rect rect-3"></div>
-		<div class="rect rect-4"></div>
-		<div class="rect rect-5"></div>
+	<div class="jet-ajax-search__spinner align-items-center justify-content-center">
+		<div class="spinner"></div>
 	</div>
 </div>
