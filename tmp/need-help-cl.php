135d24339761932206b6ce3d8f35b6cc7a001738
starlight <sergeygirchev@gmail.com>
2023-06-12 14:49:57 +0300

    UPD: need-help shortcode components

--- tmp/need-help-cl.php
+++ tmp/need-help-cl.php
@@ -2,11 +2,11 @@
 <section class="p-32 py-60 bg-100 rounded">
     <div class="container px-0">
         <h2 class="text-center m-0">Need help?</h2>
-        <div class="row row-40 justify-content-center justify-content-md-start mt-40">
-            <div class="col-sm-10 col-md-6">
+        <div class="row row-40 justify-content-center justify-content-lg-start mt-40">
+            <div class="col-sm-10 col-lg-6">
                 <?php include get_theme_file_path('tmp/need-help/fb.php'); ?>
             </div>
-            <div class="col-sm-10 col-md-6 d-none d-md-inline-flex">
+            <div class="col-sm-10 col-lg-6 d-none d-md-inline-flex">
                 <?php include get_theme_file_path('tmp/need-help/chat.php'); ?>
             </div>
         </div>
--- tmp/need-help.php
+++ tmp/need-help.php
@@ -1,14 +1,14 @@
 <section class="py-60 bg-100">
     <div class="container">
         <h3 class="text-center m-0">Need help?</h3>
-        <div class="row row-40 justify-content-center justify-content-md-start mt-40">
-            <div class="col-sm-10 col-md-6 col-xl-4">
+        <div class="row row-40 justify-content-center justify-content-lg-start mt-40">
+            <div class="col-sm-10 col-lg-6 col-xl-4">
                 <?php include get_theme_file_path('tmp/need-help/fb.php'); ?>
             </div>
-            <div class="col-sm-10 col-md-6 col-xl-4 d-none d-md-inline-flex">
+            <div class="col-sm-10 col-lg-6 col-xl-4 d-none d-md-inline-flex">
                 <?php include get_theme_file_path('tmp/need-help/chat.php'); ?>
             </div>
-            <div class="col-sm-10 col-md-6 col-xl-4">
+            <div class="col-sm-10 col-lg-6 col-xl-4">
                 <?php include get_theme_file_path('tmp/need-help/git.php'); ?>
             </div>
         </div>
